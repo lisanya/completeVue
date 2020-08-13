b061e49a9b0e66c74820b1e3d0d8faf02c0f4b1e
lisanya <1710304383@qq.com>
2020-08-13 11:15:33 +0800

    构建项目、配置全全局样式、路路由、element-ui

---
+++ complete/src/views/Login/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="login">
+        登录
+        <div class="loginContent">
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            input: ''
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.login{
+    width: 100%;
+    height: 100vh;
+    background-color: #344a5f;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .loginContent{
+        width: 50%;
+        min-height: 200px;
+        background-color: #ffffff;
+        border-radius: 5px;
+    }
+}
+</style>
